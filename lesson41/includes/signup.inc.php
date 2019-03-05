87cca6a2e0d5f395e0ad6f64c9d086aad318050e
Aaron_At_Joffrey <aglynn@joffrey.org>
2019-03-05 12:25:54 -0600

    with error handlers and echoed error messages.

--- lesson41/includes/signup.inc.php
+++ lesson41/includes/signup.inc.php
@@ -11,15 +11,21 @@ if (isset($_POST['submit'])){
 	$pwd = $_POST['pwd'];
 	//in general, always check for errors first!, if there is an error, but code checks correct, you may process a success code with erroneous inputs.
 	if (empty($first) || empty($last) || empty($email)|| empty($uid)|| empty($pwd)) {
-		header("Location: ../index.php?signup=empty");	
+		header("Location: ../index.php?signup=empty");		//by including a signup=empty we are creating a get method for signup in the index page
+		exit();
 	}	
 	else {
-		if (filter_var($email, !FILTER_VALIDATE_EMAIL)) {	//there are a lot of error handlers to consider here, and these are just a few.
-			header("Location: ../index.php?signup=invalid_email");
-		} else {
-			echo "sign up the user!";
-		}
-			
+		if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
+			header("Location: ../index.php?signup=name_char_error");
+			exit();
+		}else {
+			if (filter_var($email, !FILTER_VALIDATE_EMAIL)) {	//there are a lot of error handlers to consider here, and these are just a few.
+				header("Location: ../index.php?signup=invalid_email");
+				exit();
+			} else {
+				header("Location: ../index.php?signup=success");
+				exit();
+			}
+		}	
 	}
 }
-?>
--- lesson41/index.php
+++ lesson41/index.php
@@ -1,32 +1,52 @@
-<?php
-	include_once 'includes/dbh.inc.php';
-?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>lesson41 error handlers</title>
+<title>lesson42 error handlers</title>
 <link rel="stylesheet" type="text/css" href="includes/style.css">
 </head>
-<body>
+
+<body style="text-align:center">
 <h2> Signup </h2>
-<form action="includes/signup.inc.php" method="POST">
-	<input type="text" name="first" placeholder="FirstName">
-	<br>
-	<input type="text" name="last" placeholder="LastName">
-	<br>
-	<input type="text" name="email" placeholder="Email">
-	<br>
-	<input type="text" name="uid" placeholder="username">
-	<br>
-	<input type="text" name="pwd" placeholder="password">
-	<br>
-	<button type="submit" name="submit"> Sign up </button>
-</form>	
+<div class=box>
 	
+	<p>
+		
 
+		<form action="includes/signup.inc.php" method="POST">
+			<input type="text" name="first" placeholder="FirstName">
+			<br>
+			<input type="text" name="last" placeholder="LastName">
+			<br>
+			<input type="text" name="email" placeholder="Email">
+			<br>
+			<input type="text" name="uid" placeholder="username">
+			<br>
+			<input type="text" name="pwd" placeholder="password">
+			<br>
+			<button type="submit" name="submit"> Sign up </button>
+		</form>	
+		
+	</p>
+</div>
+<?php
+	$fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+	if (strpos($fullUrl, "signup=empty") == true) {
+		echo "<p class='error'>You did not fill in all fields!</p>";
+		exit();
+	} elseif(strpos($fullUrl, "signup=name_char_error") == true) {
+		echo "<p class='error'>You used invalid characters!</p>";
+		exit();
+	} elseif(strpos($fullUrl, "signup=invalid_email") == true) {
+		echo "<p class='error'>You used an invalid e-mail!</p>";
+		exit();
+	} elseif(strpos($fullUrl, "signup=success") == true) {		
+		echo "<p class='error'>You hvae been signed up!!</p>";
+		exit();
+	}
+ 
+?>
 </body>
-
 
 
 
