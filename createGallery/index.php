e346d27772409693cc61530532200e3ec3eac8c0
Aaron_At_Joffrey <aglynn@joffrey.org>
2019-03-11 14:02:42 -0500

    finished gallery example with good css.

--- createGallery/index.php
+++ createGallery/index.php
@@ -1,5 +1,7 @@
 <?php
 include_once "header.php";
+$_SESSION['username'] = "Admin";		//this is a cheat because I wont be implementing a complete login system here.
+
 ?>
 <body>
  <div class= "grid">
@@ -12,41 +14,40 @@ include_once "header.php";
 
 	<div class="content">
 		<div class="gallery-container">	
-			<a href="#">
-				<div>
-				<h3>This is a title</h3>
-				<p>This is a paragraph</p>
-				</div>
-			</a>
-			<a href="#">
-				<div>
-				<h3>This is a title</h3>
-				<p>This is a paragraph</p>
-				</div>
-			</a>
-			<a href="#">
-				<div>
-				<h3>This is a title</h3>
-				<p>This is a paragraph</p>
-				</div>
-			</a>
-			<a href="#">
-				<div>
-				<h3>This is a title</h3>
-				<p>This is a paragraph</p>
-				</div>
-			</a>
+		<?php
+			include_once "includes/dbh.inc.php";
+			$sql = "SELECT * FROM gallery ORDER BY  orderGallery desc;";
+			$stmt = mysqli_stmt_init($conn);
+			if (!mysqli_stmt_prepare($stmt, $sql)){
+				echo "sql statement fail";
+			} else {
+				mysqli_stmt_execute($stmt);
+				$result = mysqli_stmt_get_result($stmt);
+			}
+			while($row = mysqli_fetch_assoc($result)){
+				echo "<a href='#'>
+					<div style='background-image: url(img/gallery/". $row['imgFullNameGallery'].");'>
+					<h3>".$row['titleGallery']."</h3>
+					<p>".$row['descGallery']."</p>
+					</div>
+				</a>";
+			}
+		?>
 		</div>
-		<div class="gallery-upload">
-			<form action="includes/gallery-upload.inc.php" method="POST" enctype="multipart/form-data">
-			<input type="text" name="filename" placeholder="File name...">
-			<input type="text" name="filetitle" placeholder="Image title...">
-			<input type="text" name="filedesc" placeholder="Image desc...">
-			<input type="file" name="file">
-			<button method="submit" name="submit"> UPLOAD</button>
-			
-			</form>
-		</div>
+		<?php
+		if (isset($_SESSION['username'])){	
+			echo '<div class="gallery-upload">
+				<form action="includes/gallery-upload.inc.php" method="POST" enctype="multipart/form-data">
+				<input type="text" name="filename" placeholder="File name...">
+				<input type="text" name="filetitle" placeholder="Image title...">
+				<input type="text" name="filedesc" placeholder="Image desc...">
+				<input type="file" name="file">
+				<button method="submit" name="submit"> UPLOAD</button>
+				
+				</form>
+				</div>';
+		}
+		?>
 	</div>
 
 	<div class="footer">footer</div>
